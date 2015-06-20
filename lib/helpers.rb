1215b8424c314a5eccdf13ec904fa74d8fa83e37
leomilrib <leo@leomilrib.com>
2015-06-20 19:23:28 -0300

    Quick spacing...
    not the time yet...

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -4,20 +4,24 @@ module Helpers
     day, month, year = params[:date].split('/').map(&:to_i)
     charge_date = Date.new(year, month, day)
     cost = params[:cost].to_f
-    passes = params[:charge].to_f
+    passes = params[:balance].to_f
     schedule_date = Date.today
     days_passed = 0
+
     while schedule_date >= charge_date
       days_passed += 1 unless charge_date.saturday? or charge_date.sunday?
       charge_date = charge_date.next_day
     end
+
     expended = days_passed * cost
     passes_left = passes - expended
     qtdDiasFim = 0
+
     while passes_left > cost
       passes_left -= cost unless schedule_date.saturday? or schedule_date.sunday?
       schedule_date = schedule_date.next_day
     end
+
     {
       left: passes_left,
       recharge: schedule_date.to_s
