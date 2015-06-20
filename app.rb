0532fee1d052530b2e05446259863cdbae37490a
leomilrib <leo@leomilrib.com>
2015-06-20 18:17:13 -0300

    Edit a little from ruby... but not the time yet

--- app.rb
+++ app.rb
@@ -3,10 +3,12 @@ require 'sinatra/flash'
 require './app_helpers'
 
 get '/' do
+  @today = Date.today.strftime('%d/%m/%y')
   erb :index
 end
 
 post '/' do
+  @today = Date.today.strftime('%d/%m/%y')
   @result = calc_passes(params)
   erb :index
 end
--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,13 +1,13 @@
 module Helpers
 
   def calc_passes(params)
-    year, month, day = params[:date].split('-').map(&:to_i)
+    day, month, year = params[:date].split('/').map(&:to_i)
     charge_date = Date.new(year, month, day)
     cost = params[:cost].to_f
     passes = params[:charge].to_f
-    today = Date.today
+    schedule_date = Date.today
     days_passed = 0
-    while today >= charge_date
+    while schedule_date >= charge_date
       days_passed += 1 unless charge_date.saturday? or charge_date.sunday?
       charge_date = charge_date.next_day
     end
@@ -15,12 +15,12 @@ module Helpers
     passes_left = passes - expended
     qtdDiasFim = 0
     while passes_left > cost
-      passes_left -= cost unless today.saturday? or today.sunday?
-      today = today.next_day
+      passes_left -= cost unless schedule_date.saturday? or schedule_date.sunday?
+      schedule_date = schedule_date.next_day
     end
     {
       left: passes_left,
-      recharge: today.to_s
+      recharge: schedule_date.to_s
     }
   end
 
